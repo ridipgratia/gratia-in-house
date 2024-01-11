3f2dc1cc1df000473ad691a758bc64e86d1200a0
ridipblog <memorytemp5@gmail.com>
2024-01-11 16:54:34 +0530

    Add New url

--- client/src/context/imgContext/imageProvider.jsx
+++ client/src/context/imgContext/imageProvider.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { ImageContext } from './imageContext';
 
 const ImageProvider = ({ children }) => {
-    const serverUrl = 'https://attendance.gratiatechnology.com/uploads/';
+    // const serverUrl = 'https://attendance.gratiatechnology.com/uploads/';
     // const serverUrl = 'http://3.110.128.200:9000/uploads/';
     // const serverUrl = 'https://dakota-preventing-wales-propecia.trycloudflare.com/uploads/';
+
+    // Gratia In House 
+    const serverUrl = 'http://35.78.178.11:9000/uploads';
 
     return (
         <ImageContext.Provider value={{ serverUrl }}>
--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,7 +1,11 @@
-export const API_URL = 'http://localhost:9000/api'
+// export const API_URL = 'http://localhost:9000/api'
 // export const API_URL = 'https://attendance.takshashilascs.in/api'
 // export const API_URL = 'http://43.205.236.26:9000/api'
 // export const API_URL = 'http://3.110.128.200:9000/api'
 // export const API_URL = 'https://attendance.gratiatechnology.com/api'
 // export const API_URL = 'http://52.66.21.207:9000/api'
 // export const API_URL = 'https://dakota-preventing-wales-propecia.trycloudflare.com/api'
+
+
+// Test Gratia In House 
+export const API_URL = 'http://35.78.178.11:9000/api'
